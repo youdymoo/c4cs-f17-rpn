f20dd8f4c6ff4223336863b3462c4dcdab8c8030
caoyue <494958372@qq.com>
2017-11-03 16:36:04 -0400

    homework 8 version 3

--- test_rpn.py
+++ test_rpn.py
@@ -16,5 +16,5 @@ class TestBasics(unittest.TestCase):
         result = rpn.calculate("6 3 /")
         self.assertEqual(2, result)
     def test_expo(self):
-        result = rpn.calculate("5 3 ^")
-        self.assertEqual(8, result)
+        result = rpn.calculate("3 2 ^")
+        self.assertEqual(9, result)
